8b4a880eff7c681d853e5587d3e8361860811261
neetikatari <58066915+neetikatari@users.noreply.github.com>
2022-03-03 07:00:02 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,1 @@
+HMTL - hyper text markup language
---
+++ sketch.js
@@ -0,0 +1,13 @@
+//global variables
+
+
+function setup() {
+  createCanvas(400, 400);
+  //used to create sprite and initialize variables with a initial value
+
+}
+
+function draw() {
+  background(220);
+  
+}
